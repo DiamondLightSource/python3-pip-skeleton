79a5a7eeed71c1b04b8f58f730ee7e98f58c1f9f
coretl <coretl@users.noreply.github.com>
2022-09-09 14:31:38 +0000

    deploy: 3cde6623918ecabbc9f7e989414d952c24889f26

--- tox-direct/_static/documentation_options.js
+++ tox-direct/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev8+g7b794bb',
+    VERSION: '0.1.dev8+g3cde662',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
