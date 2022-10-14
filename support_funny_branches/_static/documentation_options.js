b68103e632457ed707672f09e0782f3e27efea67
coretl <coretl@users.noreply.github.com>
2022-10-14 14:37:05 +0000

    deploy: 193e527214be833fe8380af2e24f2cacb11ddf69

--- support_funny_branches/_static/documentation_options.js
+++ support_funny_branches/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev16+g39f6a8f',
+    VERSION: '0.1.dev16+g193e527',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
