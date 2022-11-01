d9384ec159492f2a98b0e992b1885172f0e223f5
gilesknap <gilesknap@users.noreply.github.com>
2022-11-01 08:27:16 +0000

    deploy: 0e9c7e18939753d02b56d40f8c43973e9a6c3d5a

--- lockfiles/_static/documentation_options.js
+++ lockfiles/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev21+g7b0423d',
+    VERSION: '0.1.dev22+g0e9c7e1',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
