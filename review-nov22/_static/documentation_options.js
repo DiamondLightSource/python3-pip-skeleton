db591c15df57a9a680b4810632d5e3e665a5916f
gilesknap <gilesknap@users.noreply.github.com>
2022-11-11 07:46:10 +0000

    deploy: 61dc17d9ff7fb7110bfb0a50790e230108421d10

--- review-nov22/_static/documentation_options.js
+++ review-nov22/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev43+g6b5d90d',
+    VERSION: '0.1.dev44+g61dc17d',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
