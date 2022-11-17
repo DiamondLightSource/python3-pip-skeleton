15d89cabe189d4d5439dd3c449c0678922fbbe4d
garryod <garryod@users.noreply.github.com>
2022-11-17 12:03:43 +0000

    deploy: 896be59015c3b8b6c5e6609992de26fe64dfa689

--- garryod-strict-twine-check/_static/documentation_options.js
+++ garryod-strict-twine-check/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev31+g41c0802',
+    VERSION: '0.1.dev31+g896be59',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
