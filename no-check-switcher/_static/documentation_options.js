4add73942aa34ffa9762fc09ff725023e14ed1f5
coretl <coretl@users.noreply.github.com>
2022-11-25 13:46:16 +0000

    deploy: cc681074f431029eddd7abd3677fbb6b3e81fc77

--- no-check-switcher/_static/documentation_options.js
+++ no-check-switcher/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev32+g9c364d3',
+    VERSION: '0.1.dev33+gcc68107',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
