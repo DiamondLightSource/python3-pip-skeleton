b839ffe67907327dd1477733688645866be84042
garryod <garryod@users.noreply.github.com>
2022-11-30 17:25:41 +0000

    deploy: 706d57656ad32f5b1b11e2e8a01ab3e8f50752ce

--- garryod-conda-build/_static/documentation_options.js
+++ garryod-conda-build/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev42+g9c0b792',
+    VERSION: '0.1.dev41+g706d576',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
