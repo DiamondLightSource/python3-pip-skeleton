47bcd551ccbb6bd3dd9a920283f09a5bd5e14dd0
garryod <garryod@users.noreply.github.com>
2022-12-20 10:41:58 +0000

    deploy: d5fc052ab9f996b089699eb27c9af1ba11215cac

--- remove-conda-CI/_static/documentation_options.js
+++ remove-conda-CI/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev40+g3593d67',
+    VERSION: '0.1.dev40+gd5fc052',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
