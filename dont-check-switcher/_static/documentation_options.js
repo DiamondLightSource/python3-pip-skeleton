6153ad17162b1d86ae01858161952deb3037a93b
callumforrester <callumforrester@users.noreply.github.com>
2023-01-05 09:08:17 +0000

    deploy: a483a74553d957ea34b3bf453f7c49b16ac8cd66

--- dont-check-switcher/_static/documentation_options.js
+++ dont-check-switcher/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev41+g0cb1b6f',
+    VERSION: '0.1.dev41+ga483a74',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
