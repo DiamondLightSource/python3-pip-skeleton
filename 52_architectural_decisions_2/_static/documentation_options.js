2a731ac67ba6cb9241e5f9e21c02efbbdc75a79c
evalott100 <evalott100@users.noreply.github.com>
2023-01-24 09:36:11 +0000

    deploy: 9eeac2c2cf47802b23a63d086bafde8e82688a20

--- 52_architectural_decisions_2/_static/documentation_options.js
+++ 52_architectural_decisions_2/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev47+gb0417cd',
+    VERSION: '0.1.dev48+g9eeac2c',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
