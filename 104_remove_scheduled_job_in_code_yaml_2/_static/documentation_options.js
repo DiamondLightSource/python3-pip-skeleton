41cd514348bb8e2f23529eb4da9f9c14068cbbed
evalott100 <evalott100@users.noreply.github.com>
2023-02-02 11:47:05 +0000

    deploy: c0a24bfd5654381b076c33b6db245c30404cc892

--- 104_remove_scheduled_job_in_code_yaml_2/_static/documentation_options.js
+++ 104_remove_scheduled_job_in_code_yaml_2/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev49+gae34f82',
+    VERSION: '0.1.dev48+gc0a24bf',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
