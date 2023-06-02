332e6a4a8ce7308c98b557ddb1ec9d321b400f7a
aawdls <aawdls@users.noreply.github.com>
2023-06-02 10:03:07 +0000

    deploy: bc42e05fb60b7ab5b0f8cd7f496523b17c507537

--- dls-python3-release/_static/documentation_options.js
+++ dls-python3-release/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0+untagged.gc12bfc4',
+    VERSION: '0.0+untagged.gbc42e05',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
