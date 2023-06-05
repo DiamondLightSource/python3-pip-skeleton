5a889be1b0f6ffe6dee1037e66e5b4f6c6eecea1
evalott100 <evalott100@users.noreply.github.com>
2023-06-05 08:19:24 +0000

    deploy: 33835fe4fd9584dab8621efe28988de1a2d56b00

--- change_where_we_add_coverage_so_debugging_works_intuitively/_static/documentation_options.js
+++ change_where_we_add_coverage_so_debugging_works_intuitively/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev58+g29eb967',
+    VERSION: '0.1.dev58+g33835fe',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
